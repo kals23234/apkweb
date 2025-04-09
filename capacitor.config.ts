c0b50b9dff83bfe64787bf0c48ec4e0cd97759a4
kals23234 <abnadvance@gmail.com>
2025-04-09 21:31:40 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bharadwajmainframe',
+  appName: 'bharadwajmainframe',
+  webDir: 'dist'
+};
+
+export default config;
