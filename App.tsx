d37ef8c635dd93c83b67ed727947a28764256391
kals23234 <abnadvance@gmail.com>
2025-04-10 00:27:00 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from "wouter";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/ToasterComponent";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 import FrameworksPage from "@/pages/FrameworksPage";
